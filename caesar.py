a8cb07dc3079baee934a3eec67157348345175eb
Tetiana Herei <there@softserveinc.com>
2023-02-27 19:17:19 +0200

    combined encrypt and decrypt functions of ceasar

--- caesar.py
+++ caesar.py
@@ -8,41 +8,31 @@ Have fun. Don't forget your shift :)
 
 alphabet = list(string.ascii_lowercase + string.digits + ".,:;!?@%")
 
-def encrypt(message, shift):
-    encrypted = []
+def caesar(message, shift, action):
+    result = []
+    # from the course
+    if action == "decrypt":
+        shift *= -1
+    #  
     for l in list(message):
         if l == ' ':
-            encrypted += l
+            result += l
             continue
         indx = alphabet.index(l)
-        encrypted += alphabet[(indx+shift)%len(alphabet)]
-    return ''.join(encrypted)
-
-def decrypt(message, shift):
-    decrypted = []
-    for l in list(message):
-        if l == ' ':
-            decrypted += l
-            continue
-        indx = alphabet.index(l)
-        decrypted += alphabet[(indx-shift)%len(alphabet)]
-    return ''.join(decrypted)
-
+        result += alphabet[(indx+shift)%len(alphabet)]
+    return ''.join(result)
 
 greeting()
 action = input("Type 'encrypt', 'decrypt' or 'quit':\n").lower()
-while  action != "quit":
+while  action != "quit":    
+    if action not in ["encrypt", "decrypt"]:
+        print("Wrong action choice")
+        action = input("Type 'encrypt', 'decrypt' or 'quit':\n").lower()
     message = input("Type your message:\n").lower()
     shift = input("Type the shift number\n")
     if message == '' or shift == '':
         print("Wrong input")
         action = 'quit'
         continue
-    if action == 'encrypt':
-        print(encrypt(message, int(shift)))
-    elif action == 'decrypt':
-        print(decrypt(message, int(shift)))
-    else:
-        print("Wrong action choice")
-        action = 'quit'
+    print(caesar(message, int(shift), action))
     action = input("Type 'encrypt', 'decrypt' or 'quit':\n").lower()

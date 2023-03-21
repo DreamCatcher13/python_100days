d222bf44346bfa2f774833b24e5153d3e137609a
Tetiana Herei <there@softserveinc.com>
2023-03-21 16:32:59 +0200

    started converter task

---
+++ miles_to_km.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+def converter(miles):
+    pass
+
+window = Tk()
+window.title("Miles to Km Converter")
+window.minsize(width=200, height=70)
+window.config(padx=10, pady=10)
+
+
+
+window.mainloop()

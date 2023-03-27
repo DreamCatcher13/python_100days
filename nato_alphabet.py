fb6fffc0cf7af7b5eddc1540f9a249ccdbd86d88
Tetiana Herei <there@softserveinc.com>
2023-03-27 18:51:14 +0300

    added try/except block to nato alphabet

--- nato_alphabet.py
+++ nato_alphabet.py
@@ -2,6 +2,15 @@ import pandas
 
 nato_alp = pandas.read_csv("data\\nato_alp.csv")
 nato_dict = {row.letter:row.code for (index, row) in nato_alp.iterrows()}
-word = input("Enter your word: ")
-result = [nato_dict[f"{l}"] for l in word.upper()]
-print(result)
+
+def generate_phonetic():
+    word = input("Enter your word: ")
+    try:
+        result = [nato_dict[f"{l}"] for l in word.upper()]
+    except KeyError:
+        print("Sorry, only letters in the alphabet")
+        generate_phonetic()
+    else:
+        print(result)
+
+generate_phonetic()

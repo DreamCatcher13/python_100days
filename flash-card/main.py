b44c00abad5ea22a5ed8dc65ef8bdb5f6e3730b8
Tetiana Herei <there@softserveinc.com>
2023-03-28 17:52:10 +0300

    fixed bug with button image

--- flash-card/main.py
+++ flash-card/main.py
@@ -9,15 +9,15 @@ window.title("Flash-Cards Hu-En")
 window.config(padx=25, pady=25, bg=BACKGROUND_COLOR)
 
 canvas = Canvas(width=800, height=530, highlightthickness=0, bg=BACKGROUND_COLOR)
-card = PhotoImage(file="images/card_front.png") #to transform png image to smth canvas unredstands
+card = PhotoImage(file="images/card_front.png") 
 canvas.create_image(400, 265, image=card)
 canvas.grid(column=1, row=1, columnspan=2)
 
-right = PhotoImage(file="images/right.png")
-wrong = PhotoImage(file="images/wrong.png")
+right_img = PhotoImage(file="images/right.png")
+wrong_img = PhotoImage(file="images/wrong.png")
 
-right = Button(image=right, highlightthickness=0)
-wrong = Button(image=wrong, highlightthickness=0)
+right = Button(image=right_img, highlightthickness=0)
+wrong = Button(image=wrong_img, highlightthickness=0)
 right.grid(column=2, row=2)
 wrong.grid(column=1, row=2)
 

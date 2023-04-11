3efa351e0dc62f3ca7c71c71d3adaa125094a5e5
Tetiana Herei <there@softserveinc.com>
2023-04-11 16:31:37 +0300

    added GUI to pixela habit tracker

--- habit-tracker/main.py
+++ habit-tracker/main.py
@@ -1,12 +1,82 @@
-import requests
+import requests, os, sys
 from datetime import datetime
+from tkinter import *
+from tkinter import messagebox
+
+### for pyinstaller ###
+def resource_path(relative_path):
+    try:   
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
 
 with open("token.txt", "r") as f:
     content = [l.rstrip() for l in f.readlines()]
-    token, username = content[0], content[1]
+    TOKEN, USERNAME = content[0], content[1]
 
-pixela = "https://pixe.la/v1/users"
-graph_id = "graph1"
+PIXELA = "https://pixe.la/v1/users"
+GRAPH_ID = "graph1"
+NOW = datetime.now().strftime("%Y%m%d")
+HEADERS = {
+    "X-USER-TOKEN": TOKEN
+}
+GRAPH_PARAMS = {
+    "date": NOW,
+    "quantity": "0"
+}
+
+""" posting a pixel """
+def post_a_pixel():
+    username = user.get()
+    graph_id = graph.get()
+    GRAPH_PARAMS['quantity'] = quantity.get()
+    if len(username) == 0 or len(graph_id) == 0 or len( GRAPH_PARAMS['quantity']) == 0:
+        messagebox.showinfo(title="Error", message="You should fill all the fields")
+    else:
+        endpoint = f"{PIXELA}/{username}/graphs/{graph_id}"
+        responce = requests.post(url=endpoint, json=GRAPH_PARAMS, headers=HEADERS)
+        msg = responce.json()
+        messagebox.showinfo(title="Responce", message=msg['message'])
+
+### GUI ###
+window = Tk()
+window.title("Pixela habit tracker")
+window.config(padx=15, pady=15)
+
+canvas = Canvas(width=480, height=270, highlightthickness=0)
+parrot = PhotoImage(file=resource_path("pixela.png"))
+canvas_img = canvas.create_image(240, 135, image=parrot)
+canvas.grid(column=1, row=2, columnspan=3)
+
+title = Label(text="Add a pixel :)", justify="center", font=("Agency FB", 18))
+title.grid(column=2, row=1)
+
+t_user = Label(text="Enter your username", font=("Agency FB", 14))
+t_graph = Label(text="Enter your graph ID", font=("Agency FB", 14))
+t_quantity = Label(text="Enter quantity", font=("Agency FB", 14))
+t_user.grid(column=1, row=3)
+t_graph.grid(column=1, row=4)
+t_quantity.grid(column=1, row=5)
+
+user = Entry(width=20)
+user.insert(0, USERNAME)
+graph = Entry(width=20)
+graph.insert(0, GRAPH_ID)
+quantity = Entry(width=20)
+user.grid(column=2, row=3)
+graph.grid(column=2, row=4)
+quantity.grid(column=2, row=5)
+
+add_pixel = Button(text="Add a pixel",
+                   width=15,
+                   command=post_a_pixel)
+add_pixel.grid(column=3, row=4)
+
+
+window.mainloop()
+
 
 """ creating user 
 endpoint = "https://pixe.la/v1/users"
@@ -37,23 +107,6 @@ graph_params = {
 responce = requests.post(url=endpoint, json=graph_params, headers=headers)
 print(responce.text)
 """
-
-""" posting a pixel """
-headers = {
-    "X-USER-TOKEN": token
-}
-
-now = datetime.now().strftime("%Y%m%d")
-
-graph_params = {
-    "date": now,
-    "quantity": "60"
-}
-
-endpoint = f"{pixela}/{username}/graphs/{graph_id}"
-
-responce = requests.post(url=endpoint, json=graph_params, headers=headers)
-print(responce.text) 
 
 """ update a pixel 
 headers = {

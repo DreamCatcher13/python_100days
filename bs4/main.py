12dfa15952ef7322fee2cb0b8af8cae30ff21069
Tetiana Herei <there@softserveinc.com>
2023-04-13 16:09:33 +0300

    scrapping local.html

--- bs4/main.py
+++ bs4/main.py
@@ -1,5 +1,17 @@
+from bs4 import BeautifulSoup
 
-with open("website.html", "r") as f:
+with open("website.html", "r", encoding="utf-8") as f:
     content = f.read()
 
-print(content)
+soup = BeautifulSoup(content, "html.parser")
+all_anchor_tags = soup.find_all(name="a")  #all elements
+h1 = soup.find(name="h1", id="name") #looking for one element
+section_heading = soup.find(name="h3", class_="heading")  #because 'class' is reserved !
+company_url = soup.select_one(selector="p a")  #first match by CSS selector
+items = soup.select(selector=".heading")  #by CSS class;  "#id" -- by id
+for tag in all_anchor_tags:
+    print(tag.getText())
+    print(tag.get("href")) #get value of attribute
+    print(h1.getText())
+    print(company_url)
+

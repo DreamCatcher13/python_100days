e56309a62d39a9b1aea49b64a92e2d08b2fec0a6
Tetiana Herei <there@softserveinc.com>
2023-04-13 16:49:19 +0300

    scrapping live site

--- bs4/main.py
+++ bs4/main.py
@@ -1,17 +1,20 @@
 from bs4 import BeautifulSoup
+import requests
 
-with open("website.html", "r", encoding="utf-8") as f:
-    content = f.read()
+responce = requests.get(url="https://news.ycombinator.com/")
+web_page = responce.text
+soup = BeautifulSoup(web_page, 'html.parser')
+all_span = soup.find_all(name="span", class_="titleline")
+article_texts = []
+article_links = []
+for span in all_span:
+    a = span.find("a")
+    article_texts.append(a.getText())
+    article_links.append(a.get("href"))
 
-soup = BeautifulSoup(content, "html.parser")
-all_anchor_tags = soup.find_all(name="a")  #all elements
-h1 = soup.find(name="h1", id="name") #looking for one element
-section_heading = soup.find(name="h3", class_="heading")  #because 'class' is reserved !
-company_url = soup.select_one(selector="p a")  #first match by CSS selector
-items = soup.select(selector=".heading")  #by CSS class;  "#id" -- by id
-for tag in all_anchor_tags:
-    print(tag.getText())
-    print(tag.get("href")) #get value of attribute
-    print(h1.getText())
-    print(company_url)
+for i in range(len(article_links)):
+    print(f"{article_texts[i]}\nLink: {article_links[i]}")
+
+
+
 

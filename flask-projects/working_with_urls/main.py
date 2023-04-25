8a02ff530f88aadc206e5564671393ec08a7a2c1
Tetiana Herei <there@softserveinc.com>
2023-04-25 17:53:20 +0300

    flask working with urls

--- flask-projects/working_with_urls/main.py
+++ flask-projects/working_with_urls/main.py
@@ -1,16 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
-print(__name__)
-
 @app.route('/')
 def hello_world():
     return '<h1>Hello, World!</h1>'
 
-@app.route('/bye')
-def say_bye():
-    return '<h2>Bye :)</h2>'
+@app.route('/username/<name>')
+def greet(name):
+    return f'Hello, {name}!'
 
 if __name__ == "__main__":
-    # execute only if run as a script
-    app.run()
+    app.run(debug=True)

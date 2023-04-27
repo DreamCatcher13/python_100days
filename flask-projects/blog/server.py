bc189d17114ab966aa97fc67d22badb8872527e3
Tetiana Herei <there@softserveinc.com>
2023-04-27 18:45:37 +0300

    blog not working because can't access npoint

--- flask-projects/blog/server.py
+++ flask-projects/blog/server.py
@@ -1,10 +1,28 @@
 from flask import Flask, render_template
+from datetime import datetime
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    y = datetime.now().strftime("%Y")
+    # passing value to template
+    return render_template('index.html', year=y) 
+
+@app.route("/guess/<name>")
+def guess(name):
+    agify = requests.get(f"https://api.agify.io?name={name}")
+    genderize = requests.get(f"https://api.genderize.io?name={name}")
+    age = str(agify.json()['age'])
+    gender = genderize.json()['gender']
+    return render_template('api.html', n=name, a=age, g=gender)
+
+@app.route("/blog")
+def blog():
+    blog_url = 'https://api.npoint.io/5abcca6f4e39b4955965'
+    all_posts = requests.get(url=blog_url)
+    return render_template('blog.html', posts=all_posts)
 
 if __name__ == "__main__":
     app.run(debug=True)

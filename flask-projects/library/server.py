54b8b143435a5c57146314c7caac9e9542795ef5
Tetiana Herei <there@softserveinc.com>
2023-05-11 17:03:01 +0300

    tried to make edit rating form

--- flask-projects/library/server.py
+++ flask-projects/library/server.py
@@ -50,9 +50,13 @@ def add():
 @app.route("/edit", methods=['GET', 'POST'])
 def edit():
     if request.method == "POST":
+        book_id = int(request.args.get('id'))
+        book_selected = db.get_or_404(Book, book_id)
+        book_selected.rating = int(request.form['rating'])
+        db.session.commit()
         return redirect(url_for('home'))
-    book_id = request.args.get('id')
-    book_selected = Book.query.get(book_id)
+    book_id = int(request.args.get('id'))
+    book_selected = db.get_or_404(Book, book_id)
     return render_template('edit.html', book=book_selected)
 
 if __name__ == "__main__":

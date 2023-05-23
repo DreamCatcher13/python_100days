d9dbf4433d272d882ecd166301af3d0261ce4fc5
Tetiana Herei <there@softserveinc.com>
2023-05-23 12:24:29 +0300

    flask-auth simple

--- flask-projects/flask-auth/server.py
+++ flask-projects/flask-auth/server.py
@@ -8,7 +8,15 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'any-secret-key-you-choose'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['UPLOAD_FOLDER'] = 'static'
 db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return db.get_or_404(User, user_id)
 
 ##CREATE TABLE IN DB
 class User(UserMixin, db.Model):
@@ -28,36 +36,59 @@ def home():
 @app.route('/register', methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        user = User(
-            email=request.form['email'],
-            name=request.form['name'],
-            password=request.form['password']
+        pwd = generate_password_hash(
+            password=request.form['password'],
+            method='pbkdf2:sha256',
+            salt_length=8
         )
-        db.session.add(user)
-        db.session.commit()
-        return redirect(url_for('secrets', user=user.name))
+        if User.query.filter_by(email=request.form['email']).first():
+            flash('Email already exist')
+            return render_template('register.html')
+        else:
+            user = User(
+                email=request.form['email'],
+                name=request.form['name'],
+                password=pwd
+            )
+            db.session.add(user)
+            db.session.commit()
+            login_user(user) #login new user
+            return redirect(url_for('secrets', user=user.name))
     return render_template("register.html")
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == "POST":
+        user = User.query.filter_by(email=request.form['email']).first()
+        if not user:
+            return redirect(url_for('register'))
+        pwd_match = check_password_hash(user.password, request.form['password'])
+        if pwd_match:
+            login_user(user)
+            #flash('Logged in successfully')
+            return redirect(url_for('secrets'))
+        else:
+            flash("Password doesn't match")
+            return render_template("login.html")
     return render_template("login.html")
 
 
 @app.route('/secrets')
+@login_required
 def secrets():
-    username = request.args.get('user')
-    return render_template("secrets.html", user=username.capitalize())
+    return render_template("secrets.html", user=current_user.name.capitalize())
 
 
 @app.route('/logout')
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for('home'))
 
 
 @app.route('/download')
 def download():
-    pass
+    return send_from_directory(app.config['UPLOAD_FOLDER'], 'files/cheat_sheet.pdf', as_attachment=True)
 
 
 if __name__ == "__main__":

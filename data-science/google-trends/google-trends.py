671c722da58712f25c79237708040c22d11d544b
Tetiana Herei <there@softserveinc.com>
2023-05-30 18:45:17 +0300

    continue with google data

---
+++ data-science/google-trends/google-trends.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df_tesla = pd.read_csv('TESLA Search Trend vs Price.csv')
+
+df_btc_search = pd.read_csv('Bitcoin Search Trend.csv')
+df_btc_price = pd.read_csv('Daily Bitcoin Price.csv')
+
+df_unemployment = pd.read_csv('UE Benefits Search vs UE Rate 2004-19.csv')
+
+# tesla 
+df_tesla.head()
+df_tesla.describe()
+print(f'Largest value for Tesla in Web Search: {df_tesla["TSLA_WEB_SEARCH"].max()}')
+print(f'Smallest value for Tesla in Web Search: {df_tesla["TSLA_WEB_SEARCH"].min()}')
+# unemployment
+df_unemployment.describe()
+print('Largest value for "Unemployemnt Benefits" '
+      f'in Web Search: {df_unemployment["UE_BENEFITS_WEB_SEARCH"].max()} ')
+# bitcoin
+df_btc_search.describe()
+print(f'largest BTC News Search: {df_btc_search["BTC_NEWS_SEARCH"].max()}')
+df_btc_price.head()
+# is NaN
+print(f'Missing values for Tesla?: {df_tesla.isna().values.any()}')
+print(f'Missing values for U/E?: {df_unemployment.isna().values.any()}')
+print(f'Missing values for BTC Search?: {df_btc_search.isna().values.any()}')
+print(f'Missing values for BTC price?: {df_btc_price.isna().values.any()}')
+print(f'Number of missing values: {df_btc_price.isna().values.sum()}')
+df_btc_price[df_btc_price.CLOSE.isna()]
+# remove NaN
+df_btc_price.dropna(inplace=True)
+# if date is a string
+type(df_tesla['MONTH'][0])  # > str
+# convert
+df_tesla.MONTH = pd.to_datetime(df_tesla.MONTH)
+df_btc_search.MONTH = pd.to_datetime(df_btc_search.MONTH)
+df_unemployment.MONTH = pd.to_datetime(df_unemployment.MONTH)
+df_btc_price.DATE = pd.to_datetime(df_btc_price.DATE)
+# go from daily to monthly
+df_btc_monthly = df_btc_price.resample('M', on='DATE').last()  # rule, column
+# plot
+plt.figure(figsize=(14,8), dpi=120)
+plt.title('Tesla Web Search vs Price', fontsize=18)
+plt.xticks(fontsize=14, rotation=45)
+plt.yticks(fontsize=14)
+ax1 = plt.gca()
+ax2 = ax1.twinx()
+ax1.set_ylabel('web-search', color='skyblue', fontsize=14)
+ax2.set_ylabel('price', color='#E6232E', fontsize=14)
+ax1.set_xlim([df_tesla.MONTH.min(), df_tesla.MONTH.max()])
+ax2.set_ylim((0,600))
+ax1.plot(df_tesla['MONTH'], df_tesla['TSLA_WEB_SEARCH'], color='skyblue', linewidth=3)
+ax2.plot(df_tesla['MONTH'], df_tesla['TSLA_USD_CLOSE'], color='#E6232E', linewidth=3)
+plt.show()

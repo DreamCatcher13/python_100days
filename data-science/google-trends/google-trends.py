b80c0d095b3bb38a80907887021d967618c5eec0
Tetiana Herei <there@softserveinc.com>
2023-06-01 18:45:47 +0300

    finished with google trends

--- data-science/google-trends/google-trends.py
+++ data-science/google-trends/google-trends.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 df_tesla = pd.read_csv('TESLA Search Trend vs Price.csv')
 
@@ -13,14 +14,17 @@ df_tesla.head()
 df_tesla.describe()
 print(f'Largest value for Tesla in Web Search: {df_tesla["TSLA_WEB_SEARCH"].max()}')
 print(f'Smallest value for Tesla in Web Search: {df_tesla["TSLA_WEB_SEARCH"].min()}')
+
 # unemployment
 df_unemployment.describe()
 print('Largest value for "Unemployemnt Benefits" '
       f'in Web Search: {df_unemployment["UE_BENEFITS_WEB_SEARCH"].max()} ')
+
 # bitcoin
 df_btc_search.describe()
 print(f'largest BTC News Search: {df_btc_search["BTC_NEWS_SEARCH"].max()}')
 df_btc_price.head()
+
 # is NaN
 print(f'Missing values for Tesla?: {df_tesla.isna().values.any()}')
 print(f'Missing values for U/E?: {df_unemployment.isna().values.any()}')
@@ -30,6 +34,7 @@ print(f'Number of missing values: {df_btc_price.isna().values.sum()}')
 df_btc_price[df_btc_price.CLOSE.isna()]
 # remove NaN
 df_btc_price.dropna(inplace=True)
+
 # if date is a string
 type(df_tesla['MONTH'][0])  # > str
 # convert
@@ -39,6 +44,7 @@ df_unemployment.MONTH = pd.to_datetime(df_unemployment.MONTH)
 df_btc_price.DATE = pd.to_datetime(df_btc_price.DATE)
 # go from daily to monthly
 df_btc_monthly = df_btc_price.resample('M', on='DATE').last()  # rule, column
+
 # plot
 plt.figure(figsize=(14,8), dpi=120)
 plt.title('Tesla Web Search vs Price', fontsize=18)
@@ -52,4 +58,38 @@ ax1.set_xlim([df_tesla.MONTH.min(), df_tesla.MONTH.max()])
 ax2.set_ylim((0,600))
 ax1.plot(df_tesla['MONTH'], df_tesla['TSLA_WEB_SEARCH'], color='skyblue', linewidth=3)
 ax2.plot(df_tesla['MONTH'], df_tesla['TSLA_USD_CLOSE'], color='#E6232E', linewidth=3)
+years = mdates.YearLocator()
+months = mdates.MonthLocator()
+fmt = mdates.DateFormatter('%Y')
+ax1.xaxis.set_major_locator(years)
+ax1.xaxis.set_major_formatter(fmt)
+ax1.xaxis.set_minor_locator(months)
+plt.show()
+
+# unemployment 
+plt.figure(figsize=(14,8), dpi=120)
+plt.title('Unemployment Benefits', fontsize=18)
+plt.xticks(fontsize=14, rotation=45)
+plt.yticks(fontsize=14)
+ax1 = plt.gca()
+ax2 = ax1.twinx()
+ax1.set_ylabel('FRED U/E Rate', color='purple', fontsize=14)
+ax2.set_ylabel('Search Trend', color='skyblue', fontsize=14)
+ 
+ax1.xaxis.set_major_locator(years)
+ax1.xaxis.set_major_formatter(fmt)
+ax1.xaxis.set_minor_locator(months)
+ 
+ax1.set_ylim(bottom=3, top=10.5)
+ax1.set_xlim([df_unemployment.MONTH.min(), df_unemployment.MONTH.max()])
+ 
+# Show the grid lines as dark grey lines
+ax1.grid(color='grey', linestyle='--')
+ 
+# Change the dataset used
+ax1.plot(df_unemployment.MONTH, df_unemployment.UNRATE, 
+         color='purple', linewidth=3, linestyle='--')
+ax2.plot(df_unemployment.MONTH, df_unemployment.UE_BENEFITS_WEB_SEARCH, 
+         color='skyblue', linewidth=3)
+ 
 plt.show()
